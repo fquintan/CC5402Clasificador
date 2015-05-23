48b0ec5bbf18ff9b27cdc060c4d5ef335703cb10
Felipe Quintanilla <fquintan.m@gmail.com>
2015-05-23 19:54:21 -0300

    DescriptorExtractor now receives inputDir and outputName from command line

--- src/DescriptorExtractor.cpp
+++ src/DescriptorExtractor.cpp
@@ -4,11 +4,26 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char **argv){
-	FeatureExtractor extractor;
+void usage(){
+	std::cout << "Modo de uso: extractor <inputDir> <output>" << std::endl;
+	std::cout << "inputDir: Directorio donde se encuentran las imagenes de entrenamiento" << std::endl;
+	std::cout << "output: Nombre del archivo para guardar los descriptores" << std::endl;
 
-	std::string dir = "/home/felipe/Documents/BusquedaContenido/tarea3/CC5402Clasificador/imagenes/cat_test2";
-	std::vector<std::string> files = Utils::getAllFilenames(dir);
+}
+
+int main(int argc, char **argv){
+	if (argc < 3){
+		usage();
+		return 1;
+	}
+	std::string inputDir = argv[1];
+	std::string outputName = argv[2];
+
+	std::vector<std::string> files = Utils::getAllFilenames(inputDir);
+	std::cout << "Abriendo " << inputDir << std::endl;
+	std::cout << "Se encontraron " << files.size() << " archivos" << std::endl;
+
+	FeatureExtractor extractor;
 	std::vector<cv::Mat> descriptors = extractor.extractDescriptors(files);
 
 	int i;
@@ -17,20 +32,21 @@ int main(int argc, char **argv){
 	for (i = 0; i < nFiles; i++){
 		nDescriptors += descriptors[i].rows;
 	}
-	std::cout << "Number of descriptors: " << nDescriptors << std::endl;
-	std::cout << "Dimension of descriptors: " << descriptors[0].cols << std::endl;
+	std::cout << "Descriptores locales calculados: " << nDescriptors << std::endl;
+	std::cout << "Descriptores de dimension: " << descriptors[0].cols << std::endl;
 
 	int nClusters = 15;
 	ClusterComputer clusterComputer(nClusters);
 	cv::Mat labels;
 	cv::Mat centers;
 
+	std::cout << "Calculando clusters" << std::endl;
 	clusterComputer.compute(descriptors, labels, centers);
 
 	std::ofstream outputFile;
-	std::string outputName = "descriptores.txt";
 	outputFile.open(outputName);
-	std::cout << "Saving descriptors in file: " << outputName << std::endl;
+	std::cout << "Calculando descriptores BOVW" << std::endl;
+	std::cout << "Guardando descriptores en el archivo: " << outputName << std::endl;
 
 	int c = 0;
 	for (i = 0; i < nFiles; i++){
